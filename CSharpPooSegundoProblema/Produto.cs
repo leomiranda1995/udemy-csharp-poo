412e7a054dac5ce208c5ce656ed6b2e263b24f46
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-07-12 22:35:01 -0300

    Projeto Produto

---
+++ CSharpPooSegundoProblema/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace CSharpPooSegundoProblema
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Preço: R$ {Preco}, Unidades: {Quantidade}, Total R$ {ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
