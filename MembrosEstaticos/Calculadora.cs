325b4dc2163196934d8a25b789bd2b6fb061277d
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-07-12 23:12:32 -0300

    Parte 2

---
+++ MembrosEstaticos/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MembrosEstaticos
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+        }
+    }
+}
--- MembrosEstaticos/Program.cs
+++ MembrosEstaticos/Program.cs
@@ -3,29 +3,19 @@ using System.Globalization;
 
 namespace MembrosEstaticos
 {
-    internal class Program
+    class Program
     {
-        static double Pi = 3.14;
+        
         static void Main(string[] args)
         {
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
             Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Voume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Valor de PI: {Pi.ToString("F2", CultureInfo.InvariantCulture)}");
-        }
-
-        static double Circunferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double raio)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
