8ec27709a7e883d6e2ca53ed935497ce8fdf0067
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-07-13 20:21:36 -0300

    ConstrutoresThisSobrecargaEncapsulamento

---
+++ ConstrutoresThisSobrecargaEncapsulamento/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConstrutoresThisSobrecargaEncapsulamento
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto() { }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Preço: R$ {Preco}, Unidades: {Quantidade}, Total R$ {ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ConstrutoresThisSobrecargaEncapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConstrutoresThisSobrecargaEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 900, 5);
+
+            p.Nome = "tv 4k";
+
+            Console.WriteLine(p);
+        }
+    }
+}
