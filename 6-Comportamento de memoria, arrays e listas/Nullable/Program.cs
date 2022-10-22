b806e9837404c3836a58e87447f39267155e87b8
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-10-22 10:31:29 -0300

    Aula 1 Vetores

---
+++ 6-Comportamento de memoria, arrays e listas/Nullable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operador Nullable
+
+            //Nullable<double> x = null;
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("x é nullo");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("y é nullo");
+
+            // Operador do coalescência
+            double? z = x ?? y;
+            Console.WriteLine(z);
+        }
+    }
+}
---
+++ 6-Comportamento de memoria, arrays e listas/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                soma += vet[i];
+                Console.WriteLine($"Vet[{i}] = {vet[i]}");
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine($"Media = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
